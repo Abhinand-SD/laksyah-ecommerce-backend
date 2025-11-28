35aefe3c652437a72e0c8e873a142681940d4c1a
Abhinand SD <abhinandssd90@gmail.com>
2025-11-28 15:53:54 +0530

    add addToCart functionality

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -1,13 +1,46 @@
+import userModel from "../models/userModel";
 
 
 // add products to user cart
 const addToCart = async(req,res) => {
+    try {
+        const {userId, itemId, size} = req.body
 
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData;
+
+        if(cartData[itemId]) {
+            if(cartData[itemId][size]) {
+                cartData[itemId][size] += 1
+            }else{
+                cartData[itemId][size]= 1
+            }
+        }else{
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+
+        await userModel.findByIdAndUpdate(userId, {cartData})
+        res.json({ succes: true, message: "Added To Cart"})
+
+        res,json({ succes})
+    } catch (err) {
+        console.log(err)
+        res.json({ succes: false, message: err.message})
+    }
 }
 
 // update user cart
 const updateCart = async(req,res) => {
 
+    try {
+        const {userId, itemId, size, quantity} = req.body
+
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData;
+    } catch (error) {
+        
+    }
 }
 
 // get user cart data
